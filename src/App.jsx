27b890c78621d69250cb1a969c16b4edd59d8e78
lawal rashidi <lawalrashidi95@gmail.com>
2022-07-25 08:54:14 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      
+    </div>
+  )
+}
+
+export default App
